8d374f63f165e055d15ed295a61780e5d5e4f6ed
Richard Dahl <richard@dahl.us>
2025-04-03 10:35:20 -0400

    feat: Add UserFactory for generating user test data in auth tests

--- tests/auth/conftest.py
+++ tests/auth/conftest.py
@@ -4,9 +4,29 @@
 
 import asyncio
 import pytest
+import factory
 
-from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.orm import sessionmaker
+from tests.conftest import session
 
-from uno.db import UnoBase
-from uno.db import engine, sync_engine
+from uno.auth.bases import UserBase
+from uno.auth.models import User
+
+
+class UserFactory(factory.alchemy.SQLAlchemyModelFactory):
+    class Meta:
+        model = UserBase
+        sqlalchemy_session_factory = lambda: session
+
+    full_name = factory.Faker("name")
+    email = factory.lazy_attribute(
+        lambda o: f"{o.full_name.replace(' ', '.').lower()}@example.com"
+    )
+    handle = factory.lazy_attribute(
+        lambda o: f"@{o.full_name.replace(" ", "_").lower()}"
+    )
+
+
+user = UserFactory.create()
+print("Creating user with email:", user.email)
+print("Creating user with handle:", user.handle)
+print("Creating user with full name:", user.full_name)
